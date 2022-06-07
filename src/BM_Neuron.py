FIXED gradient

src/BM_Neuron.py
@@ -100,89 +100,10 @@ class BipolarMorphological2D_Torch(nn.Module):
             return (self.kernel_shape[3], self.input_shape[1] // self.strides[0], self.input_shape[2] // self.strides[1])
 
     def forward(self, x):
-        # print(x)
-        # print(self.k1)
-        # print(self.k2)
-        # print(self.bias)
+       
         return self.BM_forward.apply(x, self.k1, self.k2, self.bias)
 
-
-class BipolarMorphological2D_Smorph(nn.Module):
-    def __init__(self, filters,
-                 input_shape,
-                 kernel_size,
-                 input_shift=0.1,
-                 padding='VALID',
-                 strides=(1, 1),
-                 **kwargs):
-        super().__init__()
-        self.filters = filters
-        self.input_shape = input_shape
-        self.kernel_size = kernel_size
-        self.input_shift = input_shift
-        self.padding = padding.upper()
-        self.strides = strides
-
-        self.build()
-       
-
-    def build(self):
-        self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
-        
-        self.k1 = torch.nn.Parameter(torch.Tensor(*self.kernel_shape))
-        self.k2 = torch.nn.Parameter(torch.Tensor(*self.kernel_shape))
-        self.bias = torch.nn.Parameter(torch.zeros(self.filters))
-
-        torch.nn.init.xavier_uniform_(self.k1)
-        torch.nn.init.xavier_uniform_(self.k2)
-
-       
-
-    def compute_output_shape(self, input_shape):
-        if self.padding == 'VALID':
-            
-            return (self.kernel_shape[3], (self.input_shape[1] - self.kernel_shape[1] + 1) // self.strides[0],
-                   (self.input_shape[2] - self.kernel_shape[0] + 1) // self.strides[1])
-        else:
-            
-            return (self.kernel_shape[3], self.input_shape[1] // self.strides[0], self.input_shape[2] // self.strides[1])
-
-    def forward(self, x):
-       
-        filter_height, filter_width, in_channels, out_channels = self.kernel_shape
-        k1_for_patches = torch.reshape(self.k1, [filter_height * filter_width * in_channels, out_channels])
-        k2_for_patches = torch.reshape(self.k2, [filter_height * filter_width * in_channels, out_channels])
-       
-        x1_patches = extract_image_patches(x, 3)
-
-        x1_patches = torch.clamp(x1_patches, min=self.input_shift)
-        x1_patches = torch.log(x1_patches)
-        
-        x1_patches = torch.unsqueeze(x1_patches, 2)
-       
-        x1k1 = x1_patches + k1_for_patches[None, :, :, None, None]
-        y11 = torch.exp(smooth_max(x1k1, dim=1))
-        
-        x1k2 = x1_patches + k2_for_patches[None, :, :, None, None]
-        y12 = torch.exp(smooth_max(x1k2, dim=1))
-
-        x2_patches = extract_image_patches(-x, 3)
-
-        x2_patches = torch.clamp(x2_patches, min=self.input_shift)
-        x2_patches = torch.log(x2_patches)
-       
-        x2_patches = torch.unsqueeze(x2_patches, 2)
-        
-        x2k1 = x2_patches + k1_for_patches[None, :, :, None, None]
-        y21 = torch.exp(smooth_max(x2k1, dim=1))
-        x2k2 = x2_patches + k2_for_patches[None, :, :, None, None]
-        y22 = torch.exp(smooth_max(x2k2, dim=1))
-        
-        y = torch.add(y11 - y12 - y21 + y22, self.bias[None, :, None, None])
-        return y
-
-
-class MorphLayer(nn.Module):
+class SMorphLayer(nn.Module):
     def __init__(self, filters,
                  input_shape,
                  kernel_size,
@@ -203,33 +124,85 @@ class MorphLayer(nn.Module):
 
         self.batch_norm = nn.BatchNorm2d(filters)
 
-        self.ln = Ln(filters,
+        self.ln = Ln(min_val=1e-9)
+
+        self.add_k1 = Add(filters,
+                 input_shape,
+                 kernel_size)
+
+        self.add_k2 = Add(filters,
+                 input_shape,
+                 kernel_size)
+
+        self.exp = Exp()
+        
+        self.smax = Smooth_Max(alpha=1)
+
+    def compute_output_shape(self, input_shape):
+        if self.padding == 'VALID':
+            return (self.kernel_shape[3], (self.input_shape[1] - self.kernel_shape[1] + 1) // self.strides[0],
+                   (self.input_shape[2] - self.kernel_shape[0] + 1) // self.strides[1])
+        else:
+            
+            return (self.kernel_shape[3], self.input_shape[1] // self.strides[0], self.input_shape[2] // self.strides[1])
+
+    def forward(self, x):
+        filter_height, filter_width, in_channels, out_channels = self.kernel_shape
+        x1_pathces = self.ln(x)
+        x2_pathces = self.ln(-x)
+
+        x1_pathces = extract_image_patches(x1_pathces, filter_height, self.strides[0])
+        x2_pathces = extract_image_patches(x2_pathces, filter_height, self.strides[0])
+
+        x1_k1 = self.add_k1(x1_pathces)
+        x1_k2 = self.add_k2(x1_pathces)
+
+        x2_k1 = self.add_k1(x2_pathces)
+        x2_k2 = self.add_k2(x2_pathces)
+       
+        y11 = self.exp(self.smax(x1_k1))
+        y12 = self.exp(self.smax(x1_k2))
+        y21 = self.exp(self.smax(x2_k1))
+        y22 = self.exp(self.smax(x2_k2))
+        y = y11 - y12 - y21 + y22 + self.bias[None, :, None, None]
+        return y
+        
+
+class MorphLayer(nn.Module):
+    def __init__(self, filters,
                  input_shape,
                  kernel_size,
-                 input_shift=1e-9,
+                 min_val=1e-9,
                  padding='VALID',
-                 strides=(1, 1))
+                 strides=(1, 1),
+                 grad_coef = 1e-2,
+                 **kwargs):
+        super().__init__()
+        self.filters = filters
+        self.input_shape = input_shape
+        self.kernel_size = kernel_size
+        self.min_val = min_val
+        self.padding = padding.upper()
+        self.strides = strides
+        self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
+
+        self.bias = torch.nn.Parameter(torch.zeros(self.filters, requires_grad=True))
+
+        self.batch_norm = nn.BatchNorm2d(filters)
+
+        self.ln = Ln(min_val=1e-9)
 
         self.add_k1 = Add(filters,
                  input_shape,
                  kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1))
+                 grad_coef)
 
         self.add_k2 = Add(filters,
                  input_shape,
                  kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1))
+                 grad_coef)
 
-        self.exp = Exp(filters,
-                 input_shape,
-                 kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1))
+        self.exp = Exp()
 
     def compute_output_shape(self, input_shape):
         if self.padding == 'VALID':
@@ -242,8 +215,15 @@ class MorphLayer(nn.Module):
     def forward(self, x):
         filter_height, filter_width, in_channels, out_channels = self.kernel_shape
         # print(f'x = {torch.mean(x)} {torch.std(x)}')
+        # print("x = ", x.shape)
         x1_pathces = self.ln(x)
         x2_pathces = self.ln(-x)
+
+        x1_pathces = extract_image_patches(x1_pathces, filter_height, self.strides[0])
+        x2_pathces = extract_image_patches(x2_pathces, filter_height, self.strides[0])
+
+        
+        # print("lnx = ", x1_pathces.shape)
 
         x1_k1 = self.add_k1(x1_pathces)
         x1_k2 = self.add_k2(x1_pathces)
@@ -252,17 +232,18 @@ class MorphLayer(nn.Module):
         x2_k2 = self.add_k2(x2_pathces)
         # print(f'x+ln = {torch.mean(x1_pathces)} {torch.std(x1_pathces)}')
         # print(f'x-ln = {torch.mean(x2_pathces)} {torch.std(x2_pathces)}')
-
-        y11 = self.exp(x1_k1)
+        # print(f'x1_k1 = {x1_k1.shape} {x1_k1.max(1)[0].shape}')
+        y11 = self.exp(x1_k1.max(1)[0])
         # print(f'y11 = {torch.mean(y11)} {torch.std(y11)}')
-        y12 = self.exp(x1_k2)
+        y12 = self.exp(x1_k2.max(1)[0])
         # print(f'y12 = {torch.mean(y12)} {torch.std(y12)}')
-        y21 = self.exp(x2_k1)
+        y21 = self.exp(x2_k1.max(1)[0])
         # print(f'y21 = {torch.mean(y21)} {torch.std(y21)}')
-        y22 = self.exp(x2_k2)
+        y22 = self.exp(x2_k2.max(1)[0])
         # print(f'y22 = {torch.mean(y22)} {torch.std(y22)}')
-
+        # print(f'y12 = {y12.shape}')
         y = y11 - y12 - y21 + y22 + self.bias[None, :, None, None]
         # print(f'y = {torch.mean(y)} {torch.std(y)}')
+        # print("y = ", y.shape)
         return y
         # return self.batch_norm(y)

src/net.py
@@ -40,12 +40,12 @@ class Smorph_Net(nn.Module):
         super().__init__()
         
         layers = []
-        layers.append(BipolarMorphological2D_Smorph(32, kernel_size = (3, 3), input_shape=shape))
+        layers.append(SMorphLayer(32, kernel_size = (3, 3), input_shape=shape))
         layers.append(nn.ReLU())
         out_shape = shape
         for i in range(depth):
             out_shape = layers[-2].compute_output_shape(input_shape=out_shape)
-            layers.append(BipolarMorphological2D_Smorph(16, kernel_size = (3, 3), input_shape=out_shape))
+            layers.append(SMorphLayer(16, kernel_size = (3, 3), input_shape=out_shape))
             layers.append(nn.ReLU())
             
         out_shape = layers[-2].compute_output_shape(input_shape=out_shape)
@@ -61,15 +61,20 @@ class Morph_Net(nn.Module):
 
     def __init__(self, depth, shape):
         super().__init__()
-        
+
+        coefs = []
+        for i in reversed(range(depth + 1)):
+            coefs.append(np.power(10, i))
+       
         layers = []
-        layers.append(MorphLayer(filters=32, kernel_size = (3, 3), input_shape=shape))
+        layers.append(MorphLayer(filters=32, kernel_size = (3, 3), input_shape=shape, grad_coef=coefs[0]))
         layers.append(nn.ReLU())
         out_shape = shape
+
         for i in range(depth):
             out_shape = layers[-2].compute_output_shape(input_shape=out_shape)
-            print(out_shape)
-            layers.append(MorphLayer(filters=16, kernel_size = (3, 3), input_shape=out_shape))
+            # print(out_shape)
+            layers.append(MorphLayer(filters=16, kernel_size = (3, 3), input_shape=out_shape, grad_coef = coefs[i + 1]))
             layers.append(nn.ReLU())
             
         out_shape = layers[-2].compute_output_shape(input_shape=out_shape)

src/train.py
@@ -17,8 +17,7 @@ from datetime import datetime
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-print(torch.cuda.is_available())
-print(device)
+print("device: ", device)
 
 def mnist(batch_size):
     
@@ -115,15 +114,15 @@ def train(model, train_dataloader, test_dataloader, criterion,
                 else:
                     mean_acc = accuracy_trace[-1]
 
-                if (n_epochs == 1):
-                    train_stat.append(loss_trace[-1], mean_acc, iter_i)
+                if (n_epochs == 1 and writer is not None):
+                    train_stat.append(loss_trace[-1], accuracy_trace[-1], iter_i)
 
                 tepoch.set_postfix(loss=loss_trace[-1], mean_accuracy = mean_acc, batch_accuracy=accuracy_trace[-1])
                 # sleep(0.1)
             if show and (iter_i + 1) % verbose == 0:
                 show_progress(epoch_i, loss_trace, accuracy_trace, x, y, y_pred)
         
-        if (n_epochs != 1):
+        if (n_epochs != 1 and writer is not None):
             train_stat.append(loss_trace[-1], mean_acc, epoch_i)
 
         # test_accuracy = test(model, test_dataloader)
@@ -139,8 +138,8 @@ def train_multilayer(depth, epochs, batch_size=100, name="BM_NET", with_logs = F
         model = Morph_Net(d, (1, 28, 28))
         model = model.to(device)
         criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-        #     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        # optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
         writer = None
         if with_logs:
@@ -151,7 +150,7 @@ def train_multilayer(depth, epochs, batch_size=100, name="BM_NET", with_logs = F
             logs = "logs/"
             writer = tb.writer.SummaryWriter(path.join(logs, model_name))
 
-        train_dataloader, test_dataloader = mnist(100)
+        train_dataloader, test_dataloader = mnist(batch_size)
 
         train_loss, train_accuracy = train(model, train_dataloader, test_dataloader,
                                     criterion, optimizer, writer,
@@ -160,5 +159,5 @@ def train_multilayer(depth, epochs, batch_size=100, name="BM_NET", with_logs = F
     return stats
 
 
-depths = [1]
-train_multilayer(depth=depths, epochs=1, with_logs=True)
+depths = [4]
+train_multilayer(depth=depths, epochs=1, batch_size=100, name="NEW_GRAD_VAL", with_logs=True)

src/utility_layers.py
@@ -4,201 +4,179 @@ from utils import *
 import torch.nn as nn
 from torch.autograd import Function
 
+USE_AUTOGRAD = False
+
 def get_hook(name):
     def print_hook(grad):
         print(f'{name} = {torch.mean(grad)}, {torch.std(grad)}')
         return grad
     return print_hook
 
-def ln_func(kernel_shape, input_shift, strides):
+def ln_func(min_val):
     class ln_func(Function):
         @staticmethod
         def forward(ctx, x):
-            filter_height, filter_width, in_channels, out_channels = kernel_shape
-            batch_size,c,h,w = x.shape
-            
-            patches = x.unfold(2, filter_height, strides[0]).unfold(3, filter_width, strides[0])
-            patches = patches.permute(0,4,5,1,2,3)
-            x1_patches = patches.reshape(batch_size,-1,patches.shape[-2], patches.shape[-1])
-
-            x1_patches = torch.clamp(x1_patches, min=input_shift)
-            x1_patches = torch.log(x1_patches)
-            x1_patches =  torch.unsqueeze(x1_patches, 2)
-            ctx.save_for_backward(x1_patches, x)
-            return x1_patches
-
-        @staticmethod
-        def backward(ctx, y_grad):
-            x1_patches, x = ctx.saved_tensors
-            # print(y_grad.shape)
-            # print(x.shape)
-            # print(x1_patches.shape)
-            return 1/x
-    return ln_func
-
-def add_func(kernel_shape):
-    class add_func(Function):
-        @staticmethod
-        def forward(ctx, x, k):
-            filter_height, filter_width, in_channels, out_channels = kernel_shape
-            k_for_patches = k.view(filter_height * filter_width * in_channels, out_channels)
-            xk = x + k_for_patches[None, :, :, None, None]
-            ctx.save_for_backward(xk, x, k)
-            return xk
-
-        @staticmethod
-        def backward(ctx, y_grad):
-            xk, x, k = ctx.saved_tensors
-            return torch.ones(x.shape, requires_grad=True), torch.ones(k.shape, requires_grad=True)
-    return add_func
-
-def exp_func():
-    class exp_func(Function):
-        @staticmethod
-        def forward(ctx, x):
-            y = torch.exp(x.max(1)[0])
+            y = torch.clamp(x, min=min_val)
+            y = torch.log(y)
+            # y =  torch.unsqueeze(y, 2)
             ctx.save_for_backward(y, x)
             return y
 
         @staticmethod
         def backward(ctx, y_grad):
             y, x = ctx.saved_tensors
-            return torch.exp(x)
-    return exp_func
+            # x = torch.clamp(x, min=min_val)
+            mask = x <= min_val
+            # print(mask)
+            del_x = 1/x
+            del_x[mask] = 0
+            # print(del_x)
+            return del_x * y_grad
+    return ln_func
 
-def smax_func(alpha):
-    class exp_func(Function):
+def add_func(kernel_shape, grad_coef):
+    class add_func(Function):
         @staticmethod
-        def forward(ctx, x):
-            ax = torch.exp(torch.mul(x, alpha))
-            s_max = torch.mul(x, ax / torch.sum(ax, dim=1, keepdims=True))
-            return torch.sum(s_max, dim=1)
+        def forward(ctx, x, k):
+            filter_height, filter_width, in_channels, out_channels = kernel_shape
+            ctx.x_shape = x.shape
+            ctx.k_shape = k.shape
+            x = torch.unsqueeze(x, 2)
+            k_for_patches = k.view(filter_height * filter_width * in_channels, out_channels)
+            k = k_for_patches[None, :, :, None, None]
+            
+            xk = x + k_for_patches[None, :, :, None, None]
+            return xk
 
         @staticmethod
         def backward(ctx, y_grad):
-            return None
+            
+            k_grad = torch.sum(y_grad, [0, -1, -2])
+            # print("k_grad ", k_grad.shape)
+            k_grad = k_grad.view(ctx.k_shape)
+            # return torch.ones(x.shape, requires_grad=True, device=y_grad.device) * y_grad, torch.ones(k.shape, requires_grad=True, device=y_grad.device) * tmp
+            x_grad = torch.sum(y_grad, [2])
+            # print("x_grad ", x_grad.shape)
+            x_grad = x_grad.view(ctx.x_shape)
+            coef = torch.numel(x_grad) * 0.1 / torch.norm(x_grad, p=2)
+            print(coef)
+            print(f'x_grad = {torch.mean(x_grad * coef)} {torch.std(x_grad * coef)}')
+            return x_grad * coef, k_grad * 1
+    return add_func
+
+def exp_func():
+    class exp_func(Function):
+        @staticmethod
+        def forward(ctx, x):
+            y = torch.exp(x)
+            ctx.save_for_backward(y, x)
+            return y
+
+        @staticmethod
+        def backward(ctx, y_grad):
+            y, x = ctx.saved_tensors
+            return y * y_grad
+    return exp_func
+
+def smax_func(alpha):
+    class smax_func(Function):
+        @staticmethod
+        def forward(ctx, x):
+            eax = torch.exp(torch.mul(x, alpha))
+            s1 = torch.sum(eax, dim=1)
+            s0 = torch.sum(x * eax, dim=1)
+            ctx.save_for_backward(x, eax, s1, s0)
+            return s0 / s1
+
+        @staticmethod
+        def backward(ctx, y_grad):
+            x, eax, s1, s0 = ctx.saved_tensors
+            tmp1 = (x * eax + eax) * torch.unsqueeze(s1, 1)
+            tmp2 = eax * torch.unsqueeze(s0, 1)
+            res = (tmp1 + tmp2) / torch.unsqueeze(s1 * s1, 1)
+            # print(res[0, :, 0, 0, 0])
+            return res * torch.unsqueeze(y_grad, 1)
     return smax_func
 
 class Ln(nn.Module):
-    def __init__(self, filters,
-                 input_shape,
-                 kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1),
+    def __init__(self, 
+                min_val = 1e-9,
                  **kwargs):
         super().__init__()
-        self.filters = filters
-        self.input_shape = input_shape
-        self.kernel_size = kernel_size
-        self.input_shift = input_shift
-        self.padding = padding.upper()
-        self.strides = strides
-        self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
+        # self.filters = filters
+        # self.input_shape = input_shape
+        # self.kernel_size = kernel_size
+        self.min_val = min_val
+        # self.padding = padding.upper()
+        # self.strides = strides
+        # self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
 
-        self.ln_forward = ln_func(self.kernel_shape, self.input_shift, self.strides)
+        self.ln_forward = ln_func(min_val=min_val)
         
     def forward(self, x):
-        filter_height, filter_width, in_channels, out_channels = self.kernel_shape
-        batch_size,c,h,w = x.shape
-        
-        patches = x.unfold(2, filter_height, self.strides[0]).unfold(3, filter_width, self.strides[0]).requires_grad_(True)
-        patches = patches.permute(0,4,5,1,2,3).contiguous().requires_grad_(True)
-        x1_patches = patches.view(batch_size,-1,patches.shape[-2], patches.shape[-1]).requires_grad_(True)
-
-        x1_patches = torch.clamp(x1_patches, min=self.input_shift).requires_grad_(True)
-        x1_patches = torch.log(x1_patches).requires_grad_(True)
-        x1_patches =  torch.unsqueeze(x1_patches, 2).requires_grad_(True)
-        # h = x1_patches.register_hook(get_hook('ln_x'))
-        return x1_patches
-        # return self.ln_forward.apply(x)
+        if USE_AUTOGRAD:
+            y = torch.clamp(x, min=self.min_val).requires_grad_(True)
+            y = torch.log(y).requires_grad_(True)
+            # y = torch.unsqueeze(y, 2).requires_grad_(True)
+            return y
+        return self.ln_forward.apply(x)
 
 class Add(nn.Module):
     def __init__(self, filters,
                  input_shape,
                  kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1),
+                 grad_coef = 1e-2,
                  **kwargs):
         super().__init__()
         self.filters = filters
         self.input_shape = input_shape
         self.kernel_size = kernel_size
-        self.input_shift = input_shift
-        self.padding = padding.upper()
-        self.strides = strides
         self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
 
         self.k = torch.nn.Parameter(torch.empty(self.kernel_shape, requires_grad=True))
         torch.nn.init.xavier_uniform_(self.k)
 
-        self.add_forward = add_func(self.kernel_shape)
+        self.add_forward = add_func(self.kernel_shape, grad_coef)
         
         
     def forward(self, x):
-        filter_height, filter_width, in_channels, out_channels = self.kernel_shape
-        # k_for_patches = torch.reshape(self.k, [filter_height * filter_width * in_channels, out_channels])
-        k_for_patches = self.k.view(filter_height * filter_width * in_channels, out_channels)
-        xk = x + k_for_patches[None, :, :, None, None]
-        # xk.register_hook(get_hook('x+k'))
-        return xk
-        # return self.add_forward.apply(x, self.k)
+        if USE_AUTOGRAD:
+            filter_height, filter_width, in_channels, out_channels = self.kernel_shape
+            x = torch.unsqueeze(x, 2)
+            k_for_patches = self.k.view(filter_height * filter_width * in_channels, out_channels)
+            xk = x + k_for_patches[None, :, :, None, None]
+            return xk
+        return self.add_forward.apply(x, self.k)
         
 
 
 class Exp(nn.Module):
-    def __init__(self, filters,
-                 input_shape,
-                 kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1),
+    def __init__(self, 
                  **kwargs):
         super().__init__()
-        self.filters = filters
-        self.input_shape = input_shape
-        self.kernel_size = kernel_size
-        self.input_shift = input_shift
-        self.padding = padding.upper()
-        self.strides = strides
-        self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
 
         self.exp_forward = exp_func()
 
     def forward(self, x):
-        # x.register_hook(get_hook('exp_x'))
-        y = torch.exp(x.max(1)[0])
-        # y.register_hook(get_hook('exp_y'))
-        return y
-        # return self.exp_forward.apply(x)
+        if USE_AUTOGRAD:
+            y = torch.exp(x)
+            return y
+        return self.exp_forward.apply(x)
 
 
 class Smooth_Max(nn.Module):
-    def __init__(self, filters,
-                 input_shape,
-                 kernel_size,
-                 input_shift=1e-9,
-                 padding='VALID',
-                 strides=(1, 1),
+    def __init__(self, 
                  alpha = 1,
                  **kwargs):
         super().__init__()
-        self.filters = filters
-        self.input_shape = input_shape
-        self.kernel_size = kernel_size
-        self.input_shift = input_shift
-        self.padding = padding.upper()
-        self.strides = strides
-        self.kernel_shape = self.kernel_size[0], self.kernel_size[1], self.input_shape[0], self.filters
-        self.alpha = 1
-
+        self.alpha = alpha
         self.max_forward = smax_func(self.alpha)
 
     def forward(self, x):
-        ax = torch.exp(torch.mul(x, self.alpha))
-        s_max = torch.mul(x, ax / torch.sum(ax, dim=1, keepdims=True))
-        return torch.sum(s_max, dim=1)
-        # return self.exp_forward.apply(x)
+        if USE_AUTOGRAD:
+            ax = torch.exp(torch.mul(x, self.alpha))
+            s_max = torch.mul(x, ax / torch.sum(ax, dim=1, keepdims=True))
+            return torch.sum(s_max, dim=1)
+        return self.max_forward.apply(x)
         
 